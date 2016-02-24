e93afed51a23177f510ea04bd5c5628b405bc8b9
Sam Tarakajian <starakajian@gmail.com>
2016-02-24 18:07:10 -0500

    Now the button says hi

--- SwiftForBabies/SwiftForBabies/ViewController.swift
+++ SwiftForBabies/SwiftForBabies/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet var hiButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sayHi(sender: UIButton) {
+        print("hi")
+    }
 
 }
 
