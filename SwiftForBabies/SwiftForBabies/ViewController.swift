94c30159ef7be02315d26c4ebcf0b018dbc5d6ee
catarak <ctarakajian@gmail.com>
2016-02-24 18:27:46 -0500

    add cat sound to button press

--- SwiftForBabies/SwiftForBabies/ViewController.swift
+++ SwiftForBabies/SwiftForBabies/ViewController.swift
@@ -7,14 +7,20 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     // MARK: Properties
     @IBOutlet var hiButton: UIButton!
+    var audioPlayer = AVAudioPlayer()
+    var catSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("cat", ofType: "wav")!)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        do { audioPlayer = try AVAudioPlayer(contentsOfURL: catSound, fileTypeHint: nil) }
+        catch let error as NSError { print(error.description) }
+        audioPlayer.prepareToPlay()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,7 +30,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sayHi(sender: UIButton) {
-        print("hi")
+        audioPlayer.play()
     }
 
 }
