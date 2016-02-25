b5798c52f8e45f51b25f36b139fd371dea0f4ab5
Sam Tarakajian <starakajian@gmail.com>
2016-02-24 19:05:03 -0500

    Added a cat that leaps majestically into frame

---
+++ SwiftForBabies/SwiftForBabies/POPBridgingHeader.h
@@ -0,0 +1,12 @@
+//
+//  POPBridgingHeader.h
+//  SwiftForBabies
+//
+//  Created by Sam Tarakajian on 2/24/16.
+//  Copyright © 2016 girlfriends. All rights reserved.
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "POP.h"
--- SwiftForBabies/SwiftForBabies/ViewController.swift
+++ SwiftForBabies/SwiftForBabies/ViewController.swift
@@ -21,7 +21,6 @@ class ViewController: UIViewController {
         do { audioPlayer = try AVAudioPlayer(contentsOfURL: catSound, fileTypeHint: nil) }
         catch let error as NSError { print(error.description) }
         audioPlayer.prepareToPlay()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,6 +30,28 @@ class ViewController: UIViewController {
 
     @IBAction func sayHi(sender: UIButton) {
         audioPlayer.play()
+        
+        performCatAnimation()
+    }
+    
+    func performCatAnimation() {
+        var catImage: UIImage
+        var catView: UIImageView
+        
+        catImage = UIImage(contentsOfFile: NSBundle.mainBundle().pathForResource("cat1", ofType: "png")!)!
+        catView = UIImageView(image: catImage)
+        catView.hidden = true
+        self.view.addSubview(catView)
+        catView.center = CGPoint(x: -catView.frame.size.width/2, y: catView.frame.size.height/2 + self.view.frame.size.height)
+        catView.hidden = false
+        
+        // Animate the cat onscreen
+        let inAnimation = POPBasicAnimation(propertyNamed: kPOPViewCenter)
+        inAnimation.toValue = NSValue(CGPoint:self.view.center)
+        inAnimation.duration = 0.3
+        catView.pop_addAnimation(inAnimation, forKey: "enter_the_cat")
+        
+        
     }
 
 }
